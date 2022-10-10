c7cbcecdbde27cf860ec72a535715db079b15513
Josue Lubaki <josuelubaki@gmail.com>
2022-10-09 23:53:37 -0400

    switch fetch request & App router to v6.4

--- src/pages/BlogPosts.jsx
+++ src/pages/BlogPosts.jsx
@@ -1,36 +1,17 @@
-import { useEffect, useState } from 'react';
-
+import { useLoaderData } from 'react-router-dom';
 import Posts from '../components/Posts';
 import { getPosts } from '../util/api';
 
 function BlogPostsPage() {
-  const [error, setError] = useState();
-  const [posts, setPosts] = useState();
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    async function loadPosts() {
-      setIsLoading(true);
-      try {
-        const posts = await getPosts();
-        setPosts(posts);
-      } catch (err) {
-        setError(err.message);
-      }
-      setIsLoading(false);
-    }
-
-    loadPosts();
-  }, []);
-
-  return (
-    <>
-      <h1>Our Blog Posts</h1>
-      {isLoading && <p>Loading posts...</p>}
-      {error && <p>{error}</p>}
-      {!error && posts && <Posts blogPosts={posts} />}
-    </>
-  );
+	const loaderData = useLoaderData();
+	return (
+		<>
+			<h1>Our Blog Posts</h1>
+			<Posts blogPosts={loaderData} />
+		</>
+	);
 }
 
 export default BlogPostsPage;
+
+export const loader = () => getPosts();
