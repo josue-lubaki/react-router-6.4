90d435f7b1bce0484228e699881cf69b1664afec
Josue Lubaki <josuelubaki@gmail.com>
2022-10-10 00:56:00 -0400

    added simulate slow api

--- src/util/api.js
+++ src/util/api.js
@@ -1,35 +1,42 @@
 export async function getPosts() {
-  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-  if (!response.ok) {
-    throw { message: 'Failed to fetch posts.', status: 500 };
-  }
-  return response.json();
+	const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+	if (!response.ok) {
+		throw { message: 'Failed to fetch posts.', status: 500 };
+	}
+	return response.json();
+}
+
+export async function getSlowPosts() {
+	await sleep(2000);
+	const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+	if (!response.ok) {
+		throw new Response('Failed to fetch posts.', { status: 500 });
+	}
+	return response.json();
 }
 
 export async function getPost(id) {
-  const response = await fetch(
-    'https://jsonplaceholder.typicode.com/posts/' + id
-  );
-  if (!response.ok) {
-    throw { message: 'Failed to fetch post.', status: 500 };
-  }
-  return response.json();
+	const response = await fetch('https://jsonplaceholder.typicode.com/posts/' + id);
+	if (!response.ok) {
+		throw { message: 'Failed to fetch post.', status: 500 };
+	}
+	return response.json();
 }
 
 export async function savePost(post) {
-  if (post.title.trim().length < 5 || post.body.trim().length < 10) {
-    throw { message: 'Invalid input data provided.', status: 422 };
-  }
+	if (post.title.trim().length < 5 || post.body.trim().length < 10) {
+		throw { message: 'Invalid input data provided.', status: 422 };
+	}
 
-  const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
-    method: 'POST',
-    body: JSON.stringify(post),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
+	const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+		method: 'POST',
+		body: JSON.stringify(post),
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	});
 
-  if (!response.ok) {
-    throw { message: 'Could not save post.', status: 500 };
-  }
+	if (!response.ok) {
+		throw { message: 'Could not save post.', status: 500 };
+	}
 }
